a37c13dbfd2c7edd0035128316ff49e1f8789875
Fabio Oliveira <fabio2352@gmail.com>
2020-01-18 15:58:13 -0200

    v1

---
+++ Aulas/basic.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python3
+
+
+
+# Tipos de dados
+
+# Inteiro = 1,2,3...10,22,100.
+# Float = 1.2,1.5.10.3.
+# String = 'texto', '100', 'fabio oliveira'.
+
+# Comentarios
+
+# - comentário em unica linha
+
+#docstring
+'''
+teste
+teste2
+teste3
+
+'''
+#comentarios em mais de uma linha
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+
+
+#Bool
+# True
+# False
+
+# Variaveis : 
+
+# nome = 'Rafael'
+# idade = 27
+# peso = 80.7
+
+# Saida de Dados 
+
+# print('Curso Python Fundamentos')
+# print(100)
+# print(10.1)
+
+
+# Entrada de dados.
+
+
+usuario = input ('Digite seu nome: ')
+print(usuario)
+
+
---
+++ Exercícios/ex_basic.py
@@ -0,0 +1,11 @@
+# faça um programa que receba 4 notas de alunos
+# some e divida por 4
+
+
+
+n1 = float(input('Digite o primeira nota: '))
+n2 = float(input('Digite o segunda nota: '))
+n3 = float(input('Digite o terceira nota nota: '))
+n4 = float(input('Digite o quarta nota: '))
+media = (n1+n2+n3+n4)/4
+print("O resulado é {}".format(media))
