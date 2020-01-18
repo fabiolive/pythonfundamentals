8e08dc8484e70c97b841bd75ad393ea7f8728a57
Fabio Oliveira <fabio2352@gmail.com>
2020-01-18 17:08:04 -0200

    v3

--- Exercícios/ex_basic.py
+++ Exercícios/ex_basic.py
@@ -1,11 +1,18 @@
 # faça um programa que receba 4 notas de alunos
 # some e divida por 4
 
-
-
+"""
 n1 = float(input('Digite o primeira nota: '))
 n2 = float(input('Digite o segunda nota: '))
 n3 = float(input('Digite o terceira nota nota: '))
 n4 = float(input('Digite o quarta nota: '))
 media = (n1+n2+n3+n4)/4
-print("O resulado é {}".format(media))
+print(f"O resulado é {media}")
+
+"""
+
+palavra = "pwd - mostra o diretorio em que você se encontra"
+print(palavra.split())
+print(palavra.upper())
+ palavra.upper
+ 
