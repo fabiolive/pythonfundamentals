a1ac4aa0cbbfcf47e2b0df080c30c92089b13330
Fabio Oliveira <fabio2352@gmail.com>
2020-01-18 18:46:10 -0200

    v3

--- Exercícios/ex_basic.py
+++ Exercícios/ex_basic.py
@@ -1,18 +1,22 @@
 # faça um programa que receba 4 notas de alunos
 # some e divida por 4
 
-"""
-n1 = float(input('Digite o primeira nota: '))
-n2 = float(input('Digite o segunda nota: '))
-n3 = float(input('Digite o terceira nota nota: '))
-n4 = float(input('Digite o quarta nota: '))
-media = (n1+n2+n3+n4)/4
-print(f"O resulado é {media}")
 
-"""
+    
+# n1 = float(input('Digite o primeira nota: '))
+# n2 = float(input('Digite o segunda nota: '))
+# n3 = float(input('Digite o terceira nota nota: '))
+# n4 = float(input('Digite o quarta nota: '))
+# media = (n1+n2+n3+n4)/4
+# print(f"O resulado é {media}")
 
-palavra = "pwd - mostra o diretorio em que você se encontra"
-print(palavra.split())
-print(palavra.upper())
- palavra.upper
- 
+
+
+# lista = ['Corinthians', [1, 2, 3, 4, 5] ,'Palmeiras', 'São Paulo', [10, 11, 12, 13, 14],'Flamengo', 'Vasco']
+# # print 3, 13, vasco
+# print(lista[1][2], lista[4][3], lista[6])
+# #print 4, São Paulo, 14
+# print(lista[1][3], lista[3], lista[4][4])
+# # print Corinthians, 2, 10, 14
+# print(lista[0], lista[1][1], lista[4][0], lista[4][4])
+
