7776cc5cad21e042debdc42c266e5805bc299b5b
Fabio Oliveira <fabio2352@gmail.com>
2020-02-01 11:05:33 -0200

    aula2

---
+++ Aulas/aula02.py
@@ -0,0 +1,16 @@
+
+
+# texto = 'isso é um texto'
+
+# texto.upper()
+# texto.lower()
+# texto.capitalize()
+# print(texto.split(" "))
+
+
+lista = ['abacaxi', 'carro', 'navio',15,5.5]
+
+lista.append('vl')
+lista.pop(0)
+lista.remove()
+print(lista[3])
