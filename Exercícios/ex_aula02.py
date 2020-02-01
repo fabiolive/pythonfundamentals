d7e9cb8c33f95f30232e664a80c9ab33f0f2b5e8
Fabio Oliveira <fabio2352@gmail.com>
2020-02-01 14:56:16 -0200

    v4

--- Exercícios/ex_aula02.py
+++ Exercícios/ex_aula02.py
@@ -20,3 +20,54 @@ dados = {
         }
     }
 }
+
+
+#Imprima as seguintes informações:
+
+
+# 1 - Nome dos Estados
+
+
+nome_sp = dados['estados']['sp']['nome']
+nome_rj = dados['estados']['rj']['nome']
+nome_mg = dados['estados']['mg']['nome']
+
+# print(f'O nome dos estados são: {nome_sp}, {nome_rj}, {nome_mg}')
+# #ou
+# print('O nome dos estados são:{}, {}, {}'.format(nome_sp, nome_rj, nome_mg))
+
+
+
+
+# Formato:
+
+#         Nome: nome do estados
+
+# # 2 - Nome dos estados e sua populações em milhões
+
+# Formato:
+#         Nome: nome do estado, População: a quantidade
+
+pop_sp = dados['estados']['sp']['população']
+pop_rj = dados['estados']['rj']['população']
+pop_mg = dados['estados']['mg']['população']
+
+print(f'A população de {nome_sp} é de {pop_sp} pessoas')
+print(f'A população do {nome_rj} é de {pop_rj} pessoas')
+print(f'A população de {nome_mg} é de {pop_mg} pessoas')
+
+
+
+# # 3 - Nome dos estados e quantidade de municipios 
+
+# Formato:
+#  Nome: nome do estado, Muncipios: a quantidade
+
+
+municipio_sp = dados['estados']['sp']['municipios']
+municipio_rj = dados['estados']['rj']['municipios']
+municipio_mg = dados['estados']['mg']['municipios']
+
+print(f'O estado {nome_sp} possui {pop_sp} municipios')
+print(f'O estado {nome_rj} possui {pop_rj} municipios')
+print(f'O estado {nome_mg} possui {pop_mg} municipios')
