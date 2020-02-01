80aadb567ad2c32ab733ac386c2a47ffd807e6fa
Fabio Oliveira <fabio2352@gmail.com>
2020-02-01 12:52:51 -0200

    v1

---
+++ Exercícios/ex_aula02.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+dados = {
+    'estados': {
+        'sp': {
+            'nome': 'São Paulo',
+            'municipios': 645,
+            'população': 44.04
+        },
+        'rj': {
+            'nome': 'Rio de Janeiro',
+            'municipios': 92,
+            'população': 16.72
+
+        },
+        'mg': {
+            'nome': 'Minas Gerais',
+            'municipios': 31,
+            'população': 20.87
+        }
+    }
+}
