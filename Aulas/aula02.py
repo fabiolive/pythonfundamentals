8dc285a2cf4b47ca13e64cd5a775218c2a40c045
Fabio Oliveira <fabio2352@gmail.com>
2020-02-01 11:52:55 -0200

    v3

--- Aulas/aula02.py
+++ Aulas/aula02.py
@@ -17,9 +17,29 @@
 # print(lista[3])
 
 
-#TUPLAS////
+# #TUPLAS////
 
-valores = ( 1,2 )
+# valores = ( 1,2 )
 
-print(type(valores))
+# print(type(valores))
+
+
+#DICIONÁRIO
+
+# ling_favorita = {'joao':'Java','Daniel':'Python','Hector':'PHP'}
+# print(ling_favorita['Daniel'])
+
+#time_favorito = {'Joao':'Corithians','Rafael':'Vasco','Camila':'Palmeiras',}
+#print(time_favorito['Camila'])
+
+#time_favorito['Rafael'] = 'Flamengo'
+# print(time_favorito)
+# print(time_favorito.values)
+# print(time_favorito.keys)
+
+dados_clientes = {'cliente':{'cl001':{'nome':'Rafeel da silva','idade':25,'telefone':'0119999999'},
+                             'cl002':{'nome':'Carla Pereira','idade':28, 'telefone':' '}}}
+
+
+
 
