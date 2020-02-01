bb07a095d4f450c0e170e49dea51b988b338854a
Fabio Oliveira <fabio2352@gmail.com>
2020-02-01 16:38:53 -0200

    v2

--- Exercícios/ex_aula02.py
+++ Exercícios/ex_aula02.py
@@ -1,73 +1,84 @@
 # -*- coding: utf-8 -*-
 
-dados = {
-    'estados': {
-        'sp': {
-            'nome': 'São Paulo',
-            'municipios': 645,
-            'população': 44.04
-        },
-        'rj': {
-            'nome': 'Rio de Janeiro',
-            'municipios': 92,
-            'população': 16.72
+# dados = {
+#     'estados': {
+#         'sp': {
+#             'nome': 'São Paulo',
+#             'municipios': 645,
+#             'população': 44.04
+#         },
+#         'rj': {
+#             'nome': 'Rio de Janeiro',
+#             'municipios': 92,
+#             'população': 16.72
 
-        },
-        'mg': {
-            'nome': 'Minas Gerais',
-            'municipios': 31,
-            'população': 20.87
-        }
-    }
-}
-
-
-#Imprima as seguintes informações:
+#         },
+#         'mg': {
+#             'nome': 'Minas Gerais',
+#             'municipios': 31,
+#             'população': 20.87
+#         }
+#     }
+# }
 
 
-# 1 - Nome dos Estados
+# #Imprima as seguintes informações:
 
 
-nome_sp = dados['estados']['sp']['nome']
-nome_rj = dados['estados']['rj']['nome']
-nome_mg = dados['estados']['mg']['nome']
+# # 1 - Nome dos Estados
+
+
+# nome_sp = dados['estados']['sp']['nome']
+# nome_rj = dados['estados']['rj']['nome']
+# nome_mg = dados['estados']['mg']['nome']
 
 # print(f'O nome dos estados são: {nome_sp}, {nome_rj}, {nome_mg}')
-# #ou
-# print('O nome dos estados são:{}, {}, {}'.format(nome_sp, nome_rj, nome_mg))
+# # #ou
+# print('O nome dos estados são:{}, {}, {}\n'.format(nome_sp, nome_rj, nome_mg))
 
 
 
 
-# Formato:
+# # Formato:
 
-#         Nome: nome do estados
+# #         Nome: nome do estados
 
-# # 2 - Nome dos estados e sua populações em milhões
+# # # 2 - Nome dos estados e sua populações em milhões
 
-# Formato:
-#         Nome: nome do estado, População: a quantidade
+# # Formato:
+# #         Nome: nome do estado, População: a quantidade
 
-pop_sp = dados['estados']['sp']['população']
-pop_rj = dados['estados']['rj']['população']
-pop_mg = dados['estados']['mg']['população']
+# pop_sp = dados['estados']['sp']['população']
+# pop_rj = dados['estados']['rj']['população']
+# pop_mg = dados['estados']['mg']['população']
 
-print(f'A população de {nome_sp} é de {pop_sp} pessoas')
-print(f'A população do {nome_rj} é de {pop_rj} pessoas')
-print(f'A população de {nome_mg} é de {pop_mg} pessoas')
-
-
-
-# # 3 - Nome dos estados e quantidade de municipios 
-
-# Formato:
-#  Nome: nome do estado, Muncipios: a quantidade
+# print(f'A população de {nome_sp} é de {pop_sp} pessoas')
+# print(f'A população do {nome_rj} é de {pop_rj} pessoas')
+# print(f'A população de {nome_mg} é de {pop_mg} pessoas\n')
 
 
-municipio_sp = dados['estados']['sp']['municipios']
-municipio_rj = dados['estados']['rj']['municipios']
-municipio_mg = dados['estados']['mg']['municipios']
 
-print(f'O estado {nome_sp} possui {pop_sp} municipios')
-print(f'O estado {nome_rj} possui {pop_rj} municipios')
-print(f'O estado {nome_mg} possui {pop_mg} municipios')
+# # # 3 - Nome dos estados e quantidade de municipios 
+
+# # Formato:
+# #  Nome: nome do estado, Muncipios: a quantidade
+
+
+# municipio_sp = dados['estados']['sp']['municipios']
+# municipio_rj = dados['estados']['rj']['municipios']
+# municipio_mg = dados['estados']['mg']['municipios']
+
+# print(f'O estado {nome_sp} possui {municipio_sp} municipios')
+# print(f'O estado {nome_rj} possui {municipio_rj} municipios')
+# print(f'O estado {nome_mg} possui {municipio_mg} municipios')
+
+
+
+
+# idade = int(input('Digite sua idade: '))
+# habilitacao = int(input('Possui Habilitação\n1- para sim\n2- para não\n > '))
+
+# if idade >= 18 and habilitacao == 1:
+#     print('Você pode dirigir')
+# else:
+#     print('Você não pode dirigir')
