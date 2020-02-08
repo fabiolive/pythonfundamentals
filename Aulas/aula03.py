173dd7af5e533da11c4c78f984ee4cbb1d60d772
Fabio Oliveira <fabio2352@gmail.com>
2020-02-08 10:43:28 -0200

    1

---
+++ Aulas/aula03.py
@@ -0,0 +1,17 @@
+#laço de reétição
+
+# frutas =['abacaxi','banana','caju']
+# for i in frutas
+#     print(i)
+
+# carros = ('golf','camaro','fusca')
+
+# for carro in carros:
+#     print(carro)
+
+
+itens = []
+
+for item in range(1,100):
+    itens.append(item)
+print(itens)
